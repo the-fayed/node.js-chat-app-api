2a86ffe9aabfb3ef3c6c96ab660272a7f21ea2d4
Ahemd Fayed <ll.ahmedfayed@gmail.com>
2024-01-14 21:27:13 +0200

    connect to mongo db

--- src/core/app.ts
+++ src/core/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 
 import { globalErrorHandler } from '../shared/middlewares/global-error-handling';
+import { dbConnection } from '../config/db-connection';
 import { ApiError } from '../shared/utils/api-error';
 
 dotenv.config();
 const app: express.Application = express();
+dbConnection();
 
 // handling not implemented routes
 app.use('*', (req, res, next) => {
