fdf51f5b8a0ee7745e886410167849e669b41ea2
Ahemd Fayed <ll.ahmedfayed@gmail.com>
2024-01-22 10:16:56 +0200

    init api feature class

---
+++ src/shared/utils/api-feature.ts
@@ -0,0 +1,66 @@
+import { Document, Query } from "mongoose";
+import { IPagination, SearchObj } from "./utils.interface";
+
+export class ApiFeature<T extends Document> {
+  private reqQuery: any;
+  private mongooseQuery: Query<T[], T>;
+  private _paginationResult: IPagination | undefined;
+
+  constructor(mongooseQuery: Query<T[], T>, reqQuery: any) {
+    this.mongooseQuery = mongooseQuery;
+    this.reqQuery = reqQuery;
+  }
+
+  paginate(documentCount: number): this {
+    let page: number = this.reqQuery.page || 1;
+    let size: number = this.reqQuery.size || 15;
+
+    let limit: number = size;
+    let skip: number = (page - 1) * limit;
+    let endIndex: number = page * limit;
+
+    const paginationResult: IPagination = {
+      current: page,
+      numberOfPages: Math.ceil(documentCount / limit),
+    };
+
+    if (endIndex < documentCount) {
+      paginationResult.next = paginationResult.current + 1;
+    }
+
+    if (skip > 0) {
+      paginationResult.prev = paginationResult.current - 1;
+    }
+
+    this.mongooseQuery = this.mongooseQuery.find().limit(limit).skip(skip);
+    this._paginationResult = paginationResult;
+
+    return this;
+  }
+
+  get PaginationResult(): IPagination | undefined {
+    return this._paginationResult;
+  }
+
+  sort(): this {
+    let sortObj: string;
+    if (this.reqQuery.sort) {
+      sortObj = this.reqQuery.sort.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.find().sort(sortObj);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.find().sort("-createdAt");
+    }
+    return this;
+  }
+
+  search(): this {
+    let keyword: string = this.reqQuery.keyword;
+    if (this.reqQuery.keyword) {
+      let searchObj: SearchObj = {
+        username: { $regex: keyword, $options: "i" },
+      };
+      this.mongooseQuery = this.mongooseQuery.find(searchObj);
+    }
+    return this;
+  }
+}
--- src/shared/utils/utils.interface.ts
+++ src/shared/utils/utils.interface.ts
@@ -1,3 +1,14 @@
 export interface CloudinaryUploadOpt {
   format: 'string';
 }
+
+export interface IPagination {
+  current: number;
+  numberOfPages: number;
+  next?: number;
+  prev?: number;
+}
+
+export interface SearchObj {
+  username: { $regex: string, $options: 'i' }
+}
